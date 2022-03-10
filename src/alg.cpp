d511cbbfa7b0c16ec4247b46d7ab355e72f1cb05
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-03-11 00:04:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,14 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int num = 0, mid = size/2;
-  if (value >= mid) {
+  if (value >= arr[mid]) {
     for (int i = mid; i < size; i++) {
       if (arr[i] == value)
         num++;
     }
   }
-  if (value < mid) {
+  if (value <= arr[mid]) {
+    
     for (int i = (mid - 1); i >= 0; i--) {
       if (arr[i] == value)
         num++;
