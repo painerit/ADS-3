c3b655740d8bbd93c1e42d834b9812f31d0bb51a
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-03-10 23:13:43 +0300

    Филимонов Павел 21-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,24 @@
 // Copyright 2021 NNTU-CS
+#include <algorithm>
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int num = 0, mid = size/2;
+  sort(arr, size);
+  if (mid < size) {
+    for (int i = mid; i < size; i++) {
+      if (arr[i] == value)
+        num++;
+    }
+  } else if (mid > size) {
+    for (int i = mid; i >= 0; i++) {
+      if (arr[i] == value)
+        num++;
+    }
+  } else if (mid == size) {
+    for (int i = 0; i < size; i++) {
+      if (arr[i] == value)
+        num++;   
+    }
+  }
+  return num;
 }
